88ecc3dc462e29b97f8003969aa111ec535cbc60
Vadim Tessier <vadimtessier@vadims-air.home>
2024-08-12 10:16:11 +0200

    fix: edited ratings templates

--- feed/views.py
+++ feed/views.py
@@ -9,9 +9,7 @@ class FeedView(LoginRequiredMixin, ListView):
     context_object_name = 'tickets'
 
     def get_queryset(self):
-        # Get IDs of users followed by the current user
         followed_users = UserFollows.objects.filter(user=self.request.user).values_list('followed_user', flat=True)
-        # Include tickets from followed users and the current user, ordered by creation date (oldest first)
         tickets = Ticket.objects.filter(
             user__in=list(followed_users) + [self.request.user]
         ).order_by('created_at')  # Ascending order
@@ -19,13 +17,11 @@ class FeedView(LoginRequiredMixin, ListView):
         # Annotate tickets with a flag indicating if the user has reviewed it
         for ticket in tickets:
             ticket.has_reviewed = Review.objects.filter(ticket=ticket, user=self.request.user).exists()
+            ticket.user_review = Review.objects.filter(ticket=ticket, user=self.request.user).first()  # Fetch user's review if it exists
 
         return tickets
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
-        # Fetch the tickets that are being displayed
         context['tickets'] = self.get_queryset()
-
         return context
